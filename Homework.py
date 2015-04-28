412d673e42711677a067c5f26aceaec0e3ff5806
RED <r.escribano1@gmail.com>
2015-04-28 19:45:00 +0000

    Created a Homework.py file. Found square root of unknown using Pythagorean Theorem

--- Homework.py
+++ Homework.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+import math #imports the math module 
+
+
 # variables for first name 
 first = "Rafael"
 # variable for last name
@@ -6,3 +10,29 @@ last = "Escribano"
 age = 31
 
 print first, last, age
+
+# new variables with floating point numbers 
+
+sideA = 12.55
+sideB = 17.85
+sideC = 21.82
+
+# square root function to calculate for sideC
+# sideC = math.sqrt(476.125);
+
+# pythagorean theorem to calculate output and length of sideC
+# print (sideA * sideA) + (sideB * sideB)
+#print sideC
+
+
+# variables for next set of problems 
+operand1 = 95
+operand2 = 64.5
+
+print operand1 + operand2 
+print operand1 - operand2
+print operand1 * operand2 
+print operand1 / operand2
+print operand1 % operand2
+
+# Thhe best method is using the variable names and including them in the expression. This helps to eliminate unnecessary repitition. So whenever a value c
